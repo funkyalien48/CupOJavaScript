8332a8932c6bcaf03ba111b160f979a06d44e577
Steven Kustov <steven@Stevens-MacBook-Pro.local>
2022-02-14 14:53:44 -0500

    Changed API keys for development

--- screens/old/Edamam.js
+++ screens/old/Edamam.js
@@ -2,9 +2,9 @@ import fire from '../../components/fire';
 // background function for api response for the barcode scanner
 export async function sendApiRequest(UPC) {
     // application identifiers for edamam
-    let APP_ID = "2d905102";
+    let APP_ID = "7eb48d51";
     //app access key for edamam
-    let API_KEY = "9a2c924f09dbc97d86d218d96821ffe7";
+    let API_KEY = "cb6a83dcd06f77840ba49f484581f3b0";
     //waits for the api server to return a response and presents a return json file with all food info
     await fetch('https://api.edamam.com/api/food-database/v2/parser?upc=' + UPC + '&app_id=' + APP_ID + '&app_key='+ API_KEY).then(response => {
         return response.json();
