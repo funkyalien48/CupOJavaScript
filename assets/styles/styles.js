aa2428da8c5e32f31b2d4e4c4ab8a16658243d3e
ChrisT <c-titus@hotmail.com>
2022-03-05 20:57:02 -0500

    fix registre button margin to auto

--- assets/styles/styles.js
+++ assets/styles/styles.js
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
         color: '#FFF',
         fontSize: 16,
         fontFamily: 'Montserrat-SemiBold',
-        marginLeft: '35%',
+        margin: 'auto',
         marginTop: '5%'
     },
     LoginButton: {
